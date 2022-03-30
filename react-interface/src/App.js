73c984a0bad2a6a10cebc26f02acd11f934a651e
Jaynish Chauhan <19it191jd@gmail.com>
2022-03-30 17:23:54 +0530

    Update App.js

--- react-interface/src/App.js
+++ react-interface/src/App.js
@@ -1,11 +1,16 @@
 import { BiArchive } from 'react-icons/bi';
-import { IoBatteryHalfOutline } from 'react-icons/io5';
+import { IoBatteryHalfOutline, IoAddCircle } from 'react-icons/io5';
+
 function App() {
   return (
-    <div className="App">
+    <div className="App container mx-auto mt-3">
       <h1>
         <IoBatteryHalfOutline />
         Your Application <BiArchive />
+      </h1>
+      <br />
+      <h1>
+        <IoAddCircle />
       </h1>
     </div>
   );
